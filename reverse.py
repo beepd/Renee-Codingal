79e74c0e71a374b988bf8b7267563566324b32e3
beepd <bipin.sd@gmail.com>
2026-03-31 19:53:12 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,7 @@
+number=input("Please Enter your own number")
+number2=""
+for i in number:
+    number2= i+ number2
+
+print("/nThe original number=",number)
+print("The reversed number=",number2)    
