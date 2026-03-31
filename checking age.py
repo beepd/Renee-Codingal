8ca851956b1fec5fe2d13afd9fe71798e0ff25ed
beepd <bipin.sd@gmail.com>
2026-03-31 16:49:55 +0530

    Add files via upload

---
+++ checking age.py
@@ -0,0 +1,5 @@
+age=int(input("Please enter your age"))
+if 10 <= age <= 20:
+    print("You has been successfully enrolled in Grade 10.")
+else:
+    print("You cannot be enrolled. Age must be between 10 and 20.")
