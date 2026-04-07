872900b4d655f4213f7d0f8109a2b426fe05a534
beepd <bipin.sd@gmail.com>
2026-04-07 15:49:52 +0530

    Add files via upload

---
+++ n value.py
@@ -0,0 +1,4 @@
+base = int(input("Enter the base number: "))
+power = int(input("Enter the power: "))
+result = pow(base, power)
+print("\nThe result of", base, "to the power of", power, "is:", result)
