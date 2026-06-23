a1af24d701d55122093b6f2a087d552169d2df66
beepd <bipin.sd@gmail.com>
2026-06-23 19:59:55 +0530

    Add files via upload

---
+++ function calculator.py
@@ -0,0 +1,9 @@
+def multiply (P*Q):
+    return
+def divide (P,Q):
+    return P/Q
+print("Please select your operation")
+print("a-Addition")
+print("b-Subtraction")
+print("c-Multiply")
+print("d-Divide")
