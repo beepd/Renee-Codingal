a664058732f1b81c33a04fa66ba47ba4a0f60ec2
beepd <bipin.sd@gmail.com>
2026-07-16 18:52:39 +0530

    Add files via upload

---
+++ school snack counter.py
@@ -0,0 +1,16 @@
+box_A = {"Apple", "Chips", "Cookie"}
+box_B = {"Banana", "Chips", "Juice"}
+print("Box A:", box_A)
+print("Box B:", box_B)
+box_A.add("Pizza")
+box_B.add("Popcorn")
+print("\nUpdated Box A:", box_A)
+print("\nUpdated Box B:", box_B)
+snack_counts = [3, 4, 2, 5]
+print("\nInitial snack counts:", snack_counts)
+snack_counts.append(6)
+print("After adding a value:", snack_counts)
+count_of_fours = snack_counts.count(4)
+print("\nNumber of times '4' appears:", count_of_fours)
+snack_counts.reverse()
+print("Reversed snack counts:", snack_counts)
